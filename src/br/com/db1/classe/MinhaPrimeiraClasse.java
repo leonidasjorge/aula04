622445103cecbc939358ee13b8aca54cce6b78cf
Leônidas <leonidasjorge@hotmail.com>
2018-05-11 16:40:18 -0300

    alterando a frase.

--- src/br/com/db1/classe/MinhaPrimeiraClasse.java
+++ src/br/com/db1/classe/MinhaPrimeiraClasse.java
@@ -3,6 +3,6 @@ package br.com.db1.classe;
 public class MinhaPrimeiraClasse {
 
 	public static void main(String[] args) {
-		System.out.println("que aula joia!");
+		System.out.println("que aula MARAVILHOSA!");
 	}
 }
